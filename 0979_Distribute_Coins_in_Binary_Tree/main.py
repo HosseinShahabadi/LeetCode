55a3958d19101464936df743114e2d5edb7aa75d
Hossein Shahabadi <hosseinshahabadi.dev@gmail.com>
2024-05-18 23:25:07 +0330

    Time: 29 ms (95.30%), Space: 16.44 MB (90.80%)

---
+++ 0979_Distribute_Coins_in_Binary_Tree/main.py
@@ -0,0 +1,17 @@
+class Solution:
+    def distributeCoins(self, root: Optional[TreeNode]) -> int:
+        def dfs(root):
+            if root is None:
+                return 0
+            
+            left = dfs(root.left)
+            right = dfs(root.right)
+            
+            nonlocal ans
+            ans += (abs(left) + abs(right))
+            return left + right + root.val - 1
+
+        ans = 0
+        dfs(root)
+        return ans
+        
