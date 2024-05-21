8640933b18c2247b2823e10795a3da7737eb2e78
Hossein Shahabadi <hosseinshahabadi.dev@gmail.com>
2024-05-22 00:40:20 +0330

    Time: 64 ms (96.84%), Space: 17.89 MB (90.76%)

--- 0026_Remove_Duplicates_from_Sorted_Array/main.py
+++ 0026_Remove_Duplicates_from_Sorted_Array/main.py
@@ -1,5 +1,5 @@
 class Solution:
-    def removeDuplicates(self, nums: list[int]) -> int:
+    def removeDuplicates(self, nums: List[int]) -> int:
         k = len(nums)
         for i in range(1, len(nums)):
             if nums[i] == nums[i-1]:
