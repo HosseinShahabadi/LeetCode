39e05155dca398572a74344c9699559549c1654f
Hossein Shahabadi <hosseinshahabadi.dev@gmail.com>
2024-05-24 21:48:05 +0330

    Time: 53 ms (46.26%), Space: 16.45 MB (99.55%)

---
+++ 1255_Maximum_Score_Words_Formed_by_Letters/main.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxScoreWords(self, words: list[str], letters: list[str], score: list[int]) -> int:
+        count = [0] * 26
+        letters_count = [0] * 26
+        
+        for letter in letters:
+            count[ord(letter) - ord('a')] += 1
+        
+        self.result = 0
+        self.backtracking(words, score, count, letters_count, 0, 0)
+        return self.result
+    
+    def backtracking(self, words, score, count, letters_count, pos, temp):
+        for i in range(26):
+            if letters_count[i] > count[i]:
+                return
+        
+        self.result = max(self.result, temp)
+        
+        for i in range(pos, len(words)):
+            for c in words[i]:
+                letters_count[ord(c) - ord('a')] += 1
+                temp += score[ord(c) - ord('a')]
+            
+            self.backtracking(words, score, count, letters_count, i + 1, temp)
+            
+            for c in words[i]:
+                letters_count[ord(c) - ord('a')] -= 1
+                temp -= score[ord(c) - ord('a')]
