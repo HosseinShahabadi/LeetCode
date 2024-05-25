3c2003ba83dfa1fc7376799b6021f564ee7379ce
Hossein Shahabadi <hosseinshahabadi.dev@gmail.com>
2024-05-25 13:09:53 +0330

    Time: 663 ms (98.88%), Space: 27.2 MB (95.45%)

---
+++ 0121_Best_Time_to_Buy_and_Sell_Stock/main.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
+        
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            else:
+                potential_profit = price - min_price
+                if potential_profit > max_profit:
+                    max_profit = potential_profit
+        
+        return max_profit
+    
