30ca86e1497412a7f7c0325d7cf28cf5af2c1629
Hossein Shahabadi <hosseinshahabadi.dev@gmail.com>
2024-05-28 01:10:51 +0330

    Time: 31 ms (96.93%), Space: 16.78 MB (98.33%)

--- 0274_H-Index/main.py
+++ 0274_H-Index/main.py
@@ -1,11 +1,29 @@
+# O(n log n) time complexity:
+# class Solution:
+#     def hIndex(self, citations: list[int]) -> int:
+#         n = len(citations)
+#         citations.sort()
+#         for i in range(n, -1, -1):
+#             if n-i < n and citations[n-i] >= i:
+#                 return i
+        
+#         return 0
+
+
+# O(n) time complexity:
 class Solution:
     def hIndex(self, citations: list[int]) -> int:
         n = len(citations)
-        citations.sort()
-        print(citations)
-        for i in range(n, -1, -1):
-            if n-i < n and citations[n-i] >= i:
-                return i
+        special_list = [0] * (n+1)
+
+        for index, value in enumerate(citations):
+            if value > n :
+                special_list[n] += 1
+            else:
+                special_list[value] += 1
         
-        return 0
-    
+        total = 0
+        for i in range(n, -1, -1):
+            total += special_list[i]
+            if total >= i:
+                return i
