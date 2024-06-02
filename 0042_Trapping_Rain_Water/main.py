6d2c2d40f351615658ed6abc1f5a6a757ae2e00d
Hossein Shahabadi <hosseinshahabadi.dev@gmail.com>
2024-06-02 14:26:24 +0330

    Time: 99 ms (68.86%), Space: 18.40 MB (91.13%)

--- 0042_Trapping_Rain_Water/main.py
+++ 0042_Trapping_Rain_Water/main.py
@@ -1,6 +1,7 @@
 class Solution:
     def trap(self, height: list[int]) -> int:
-        # Credit: This solution was provided by Mohammed_Raziullah_Ansari.
+        # Credit: The original solution was provided by Mohammed_Raziullah_Ansari.
+        # It has been optimized slightly to prevent an extra loop.
         # Your contribution is greatly appreciated. Thank you!
         n = len(height)
         if n == 0:
@@ -9,19 +10,16 @@ class Solution:
         left = [0] * n
         right = [0] * n
         
-        # Fill left array
-        left[0] = height[0]
-        for i in range(1, n):
-            left[i] = max(left[i - 1], height[i])
-        
         # Fill right array
         right[n - 1] = height[n - 1]
         for i in range(n - 2, -1, -1):
             right[i] = max(right[i + 1], height[i])
-        
+
         # Calculate trapped water
-        trappedWater = 0
-        for i in range(n):
+        left[0] = height[0]
+        trappedWater = min(left[0], right[0]) - height[0]
+        for i in range(1, n):
+            left[i] = max(left[i - 1], height[i])
             trappedWater += min(left[i], right[i]) - height[i]
         
         return trappedWater
