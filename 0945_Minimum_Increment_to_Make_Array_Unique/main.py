5603d85aa4d7bd7ec62bd1136ab8ca40bbd0acf3
Hossein Shahabadi <hosseinshahabadi.dev@gmail.com>
2024-06-14 21:16:39 +0330

    Time: 643 ms (56.31%), Space: 30.10 MB (93.92%)

---
+++ 0945_Minimum_Increment_to_Make_Array_Unique/main.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minIncrementForUnique(self, nums: list[int]) -> int:
+        n = len(nums)
+        nums.sort()
+        ans = 0
+        for i in range(1, n):
+            if nums[i] <= nums[i - 1]:
+                ans += nums[i - 1] - nums[i] + 1
+                nums[i] = nums[i - 1] + 1
+        
+        return ans
